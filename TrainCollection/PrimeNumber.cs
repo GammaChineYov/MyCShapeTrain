1fbe6d9320a3be254e8cf4c0f024df1691830f53
GammaChineYov <gammachineyov@139.com>
2022-06-04 11:29:29 +0800

    PrimeTestFinish

--- TrainCollection/PrimeNumber.cs
+++ TrainCollection/PrimeNumber.cs
@@ -1,15 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
 namespace TrainCollection4
 {
-    class Programe
+    public class Programe
     {
         public static void Main()
         {
+            int[] inputVals = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+            int[] rightVals = { 2, 3, 5, 7, 11};
+            var sw = new Stopwatch();
+            Console.WriteLine("Check is Prime");
+            sw.Start();
+            for (int i = 0; i < inputVals.Length; i++)
+            {
+                int input = inputVals[i];
+                bool isPrime = CheckIsPrime(input);
+                if (rightVals.Contains(input) != isPrime)
+                {
+                    Console.WriteLine($"Error Input: {input}, result: {isPrime}");
+                }
+            }
+            Console.WriteLine($"Check Complete, cd:{sw.ElapsedMilliseconds}");
 
+            Console.WriteLine("ALTS Prime method");
+            int maxVal = inputVals.Max();
+            bool[] primesFlags = GetPrimeFlagsFor(maxVal);
+            for (int i = 0; i < primesFlags.Length; i++)
+            {
+                if (rightVals.Contains(i) != primesFlags[i])
+                {
+                    Console.WriteLine($"Error: Input:{i}, Output{primesFlags[i]}");
+                }
+            }
+            Console.WriteLine($"ALTS Finish, cd: {sw.ElapsedMilliseconds}");
+
+            Console.ReadKey();
+
+
+        }
+
+        public static bool[] GetPrimeFlagsFor(int maxVals)
+        {
+            bool[] result = new bool[maxVals];
+            for (int i = 2; i < result.Length; i++) result[i] = true;
+            int prime = 2;
+            while (prime < result.Length)
+            {
+                SetFalseOfMultiple(result, prime);
+
+                prime = GetNextPrime(result, prime);
+            }
+            return result;
+        }
+
+        private static void SetFalseOfMultiple(bool[] primeFlags, int prime)
+        {
+            for (int i = prime * prime; i < primeFlags.Length; i += prime)
+            {
+                primeFlags[i] = false;
+            }
+        }
+
+        private static int GetNextPrime(bool[] primeFlags, int prime)
+        {
+            prime++;
+            while (prime < primeFlags.Length && !primeFlags[prime])
+            {
+                prime++;
+            }
+
+            return prime;
+        }
+
+        public static bool CheckIsPrime(int input)
+        {
+            if (input < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i < input; i++)
+            {
+                if (input % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 
---
+++ TrainCollectionIsPrimeTests/ProgrameTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainCollection4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainCollection4.Tests
+{
+    [TestClass()]
+    public class ProgrameTests
+    {
+        [TestMethod()]
+        public void CheckIsPrimeTest()
+        {
+            Assert.IsFalse(Programe.CheckIsPrime(1));
+            Assert.IsTrue(Programe.CheckIsPrime(2));
+            Assert.IsTrue(Programe.CheckIsPrime(3));
+            Assert.IsFalse(Programe.CheckIsPrime(4));
+            Assert.IsTrue(Programe.CheckIsPrime(5));
+            Assert.IsFalse(Programe.CheckIsPrime(6));
+            Assert.IsTrue(Programe.CheckIsPrime(7));
+        }
+
+        [TestMethod()]
+        public void GetPrimeFlagsForTest()
+        {
+            bool[] result = Programe.GetPrimeFlagsFor(13);
+            Assert.AreEqual(result[0], false);
+            Assert.AreEqual(result[1], false);
+            Assert.AreEqual(result[2], true);
+            Assert.AreEqual(result[3], true);
+            Assert.AreEqual(result[4], false);
+            Assert.AreEqual(result[5], true);
+            Assert.AreEqual(result[6], false);
+            Assert.AreEqual(result[7], true);
+            Assert.AreEqual(result[8], false);
+        }
+    }
+}
