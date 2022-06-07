df8b117ae3c1a1385c783923a111582477475baa
GammaChineYov <gammachineyov@139.com>
2022-06-08 07:58:38 +0800

    New practice:prime Number

---
+++ TrainCollection/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace TrainCollection
+{
+	public class Programe
+    {
+
+        public static void Main()
+        {
+
+        }
+    }
+
+}
