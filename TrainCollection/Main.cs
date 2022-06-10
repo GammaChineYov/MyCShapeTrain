14150c6cc6523ba598697cead8e682e854db2df0
GammaChineYov <gammachineyov@139.com>
2022-06-10 09:05:13 +0800

    new practice: prime

--- TrainCollection/Main.cs
+++ TrainCollection/Main.cs
@@ -49,18 +49,6 @@ namespace TrainCollection
             }
         }
 
-        public static bool IsPrime(int input)
-        {
-            if (input < 2) return false;
-            for (int i = 2; i < input; i++)
-            {
-                if (input % i == 0)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
     }
 
 }
